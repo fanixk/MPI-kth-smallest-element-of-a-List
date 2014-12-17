abfaef402defbaa94363c871ba5f930c601d6b33
Spyros Desyllas <sdesyllas@hotmail.com>
2014-12-17 13:15:02 +0200

    Initial Commit and MPI Hello World

---
+++ MpiKthElement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPI;
+
+namespace MpiKthElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (new MPI.Environment(ref args))
+            {
+                // MPI program goes here!
+                Console.WriteLine("Hello, World! from rank " + Communicator.world.Rank
+                  + " (running on " + MPI.Environment.ProcessorName + ")");
+            }
+            Console.ReadLine();
+        }
+    }
+}
